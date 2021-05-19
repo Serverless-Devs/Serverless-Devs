9d9510780a0b4756cca319e209fd0836e35c4f26
anycodes <jiangyu.ly@alibaba-inc.com>
2021-05-19 18:53:40 +0800

    fix status code

--- src/core/component/hook.ts
+++ src/core/component/hook.ts
@@ -28,37 +28,25 @@ export class Hook {
 
     async executePreHook() {
         if (this.preHooks.length > 0) {
-            logger.info('Start the pre-hook');
+            logger.info('Start the pre-action');
             for (let i = 0; i < this.preHooks.length; i++) {
-                (async (index) => {
-                    let hookIndex = index;
-                    logger.info(`[Hook / Plugin] ${this.preHooks[hookIndex].Hook || this.preHooks[hookIndex].Plugin}`);
-                    await this.executeByConfig(this.preHooks[hookIndex]);
-                })(i);
+                let hookIndex = i;
+                logger.info(`Action: ${this.preHooks[hookIndex].Hook || this.preHooks[hookIndex].Plugin}`);
+                await this.executeByConfig(this.preHooks[hookIndex]);
             }
-            logger.info('End the pre-hook');
+            logger.info('End the pre-action');
         }
     }
 
     async executeAfterHook() {
         if (this.afterHooks.length > 0) {
-            logger.info('Start the after-hook');
+            logger.info('Start the after-action');
             for (let i = 0; i < this.afterHooks.length; i++) {
-                // logger.info(`[Hook / Plugin] ${this.afterHooks[i].Hook || this.afterHooks[i].Plugin}`);
-                // try {
-                (async (index) => {
-                    let hookIndex = index;
-                    await await this.executeByConfig(this.afterHooks[hookIndex]);
-                })(i);
-                // } catch (ex) {
-                //   process.env['project_error'] = String(true)
-                //   const thisMessage = `> Execute Error: ${this.preHooks[i].Hook || this.preHooks[i].Plugin}\n${ex}`
-                //   const tempMessage = process.env['project_error_message'] ? process.env['project_error_message'] + "\n" : ""
-                //   process.env['project_error_message'] = tempMessage + thisMessage
-                //   logger.error(`[Hook / Plugin] [Error]: ${ex.stdout || ex.stderr}`);
-                // }
+                let hookIndex = i;
+                logger.info(`Action: ${this.afterHooks[hookIndex].Hook || this.afterHooks[hookIndex].Plugin}`);
+                await this.executeByConfig(this.afterHooks[hookIndex]);
             }
-            logger.info('End the after-hook');
+            logger.info('End the after-action');
         }
     }
 
@@ -67,14 +55,9 @@ export class Hook {
         const cwdPath = executePath ? path.resolve(currentDir, executePath) : currentDir;
         if (fs.existsSync(cwdPath) && fs.lstatSync(cwdPath).isDirectory()) {
             process.env['next-command-execute-flag'] = 'true';
-            logger.info('Executing ...');
             const result = spawnSync(command, [], {cwd: cwdPath, stdio: 'inherit', shell: true});
             if (result && result.status !== 0) {
-                logger.error(`Failed to execute:\n
-  ❌ Message: Action [${command}] run error.
-  🧭 You can check whether your action command are correct. If the exit code is not 0, it is considered an error.
-  😈 If you have questions, please tell us: https://github.com/Serverless-Devs/Serverless-Devs/issues\n`)
-                process.exit(-1);
+                throw new Error(`Action [${command}] run error.`)
             }
         }
     }
