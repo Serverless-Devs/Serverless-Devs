0f376bc1d4d15fe8475882ccb88ba62df7ad8aa0
wulinsheng123 <409187100@qq.com>
2021-07-14 11:53:37 +0800

    fix: code increases the semicolon

--- test/utils/index.test.ts
+++ test/utils/index.test.ts
@@ -2,7 +2,7 @@ import { getYamlPath } from '../../src/utils/index'
 import os from 'os'
 describe('module utils', () => {
   it('test getYamlPath methods that get yaml folder', () => {
-    const v = getYamlPath(`${os.homedir()}/.s`, 'access')
-    expect(v).toBeTruthy()
+    const v = getYamlPath(`${os.homedir()}/.s`, 'access');
+    expect(v).toBeTruthy();
   });
-})
+});
--- test/utils/storage.test.ts
+++ test/utils/storage.test.ts
@@ -4,15 +4,15 @@ import { getHomeDir, getHistoryFile } from '../../src/utils/storage'
 
 describe('module storage', () => {
   it('test getHomeDir methods', () => {
-    const path = getHomeDir()
-    expect(path).toContain('.s')
-    const v = fs.existsSync(`${os.homedir()}/.s`)
-    expect(v).toBeTruthy()
+    const path = getHomeDir();
+    expect(path).toContain('.s');
+    const v = fs.existsSync(`${os.homedir()}/.s`);
+    expect(v).toBeTruthy();
   });
   it('test getHistoryFile methods', () => {
-    const path = getHistoryFile()
-    expect(path).toContain('history')
-    const v = fs.existsSync(`${os.homedir()}/.s/history`)
-    expect(v).toBeTruthy()
+    const path = getHistoryFile();
+    expect(path).toContain('history');
+    const v = fs.existsSync(`${os.homedir()}/.s/history`);
+    expect(v).toBeTruthy();
   });
-})
+});
