4cd824571fb7f4c2d09053690a73d3cbf5d64450
xsahxl <xsahxl@126.com>
2021-09-24 17:06:02 +0800

    feat: 🎸 handleError add analysis

--- src/error/index.ts
+++ src/error/index.ts
@@ -1,5 +1,6 @@
 /** @format */
 import { red, bgRed } from '../utils/common';
+import { getConfig } from '../utils/handler-set-config';
 import getCore from '../utils/s-core';
 const { colors, report, getMAC } = getCore();
 const pkg = require('../../package.json');
@@ -17,8 +18,11 @@ function underline(prefix: string, link: string) {
 }
 export function handleError(error: Error, prefix = 'Message:', exit = true) {
   const traceId = `${pid}${Date.now()}`;
-  console.log(red(`✖ ${prefix}`));
-  console.log(colors.gray(`\nTraceId:     ${traceId}`));
+  console.log(red(`✖ ${prefix}\n`));
+  const analysis = getConfig('analysis');
+  if (analysis !== 'disable') {
+    console.log(colors.gray(`TraceId:     ${traceId}`));
+  }
   console.log(
     colors.gray(
       `Environment: ${pkg.name}: ${pkg.version}, ${process.platform}-${process.arch}, node-${process.version}`,
@@ -29,7 +33,10 @@ export function handleError(error: Error, prefix = 'Message:', exit = true) {
   console.log(underline('Issues:      ', 'https://github.com/Serverless-Devs/Serverless-Devs/issues\n'));
   console.log(bgRed('ERROR:'));
   console.log(error);
-  console.log(colors.gray(`\nPlease copy traceId: ${traceId} and join Dingding group: 33947367 for consultation.`));
+  console.log('');
+  if (analysis !== 'disable') {
+    console.log(colors.gray(`Please copy traceId: ${traceId} and join Dingding group: 33947367 for consultation.`));
+  }
   console.log(colors.gray("Run again with the '--debug' option or 's -h' to get more logs."));
   report({
     type: 'jsError',
