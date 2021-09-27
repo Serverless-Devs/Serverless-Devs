6665a1d1c1e2bb57dc620b58a8d16a2ff692f97b
xsahxl <xsahxl@126.com>
2021-09-27 19:00:23 +0800

    fix: 🐛 #211 cli中优先获取 缓存中的core逻辑问题

--- src/utils/s-core.ts
+++ src/utils/s-core.ts
@@ -1,15 +1,20 @@
-/** @format */
-
 import os from 'os';
 import path from 'path';
 import fs from 'fs-extra';
+import { get } from 'lodash';
+const semver = require('semver');
+const pkg = require('../../package.json');
 
 const corePath = path.join(os.homedir(), '.s', 'cache', 'core');
 const corePackagePath = path.join(corePath, 'package.json');
 
 function getCore() {
   if (fs.existsSync(corePackagePath)) {
-    return require(corePath);
+    const localCoreVersion = get(pkg, ['dependencies', '@serverless-devs/core']);
+    const cacheCoreVersion = getCoreVersion();
+    return semver.gt(localCoreVersion.replace(/\^/, ''), cacheCoreVersion)
+      ? require('@serverless-devs/core')
+      : require(corePath);
   }
   return require('@serverless-devs/core');
 }
