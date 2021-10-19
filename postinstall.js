bb5571f98b80ed6ac9e6ac6446581ab9c71c6b07
xsahxl <xsahxl@126.com>
2021-10-19 16:11:05 +0800

    feat: 🎸 #231 安装时候默认集成组件 devsapp开头

--- postinstall.js
+++ postinstall.js
@@ -1,10 +1,7 @@
 const core = require('./lib/utils/core').default;
 (async () => {
-  await core.loadComponent('fc');
   await core.loadComponent('devsapp/fc');
-  await core.loadComponent('fc-deploy');
   await core.loadComponent('devsapp/fc-deploy');
-  await core.loadComponent('fc-build');
   await core.loadComponent('devsapp/fc-build');
   process.exit();
 })().catch(() => {
