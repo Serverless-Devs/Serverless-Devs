1f1cb710aa25321554d4162c5ad93fc037b52866
heimanba <371510756@qq.com>
2021-10-27 22:53:14 +0800

    Update postbuild.sh

--- shell/postbuild.sh
+++ shell/postbuild.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-esbuild lib/index.js --bundle --log-level=error --minify --external:@serverless-devs/core  --platform=node --format=cjs --target=es2020 --allow-overwrite --outfile=lib/index.js
-esbuild lib/init/index.js --bundle --log-level=error --minify --external:@serverless-devs/core  --platform=node --format=cjs --target=es2020 --allow-overwrite --outfile=lib/init/index.js
-esbuild lib/cli/index.js --bundle --log-level=error --minify --external:@serverless-devs/core  --platform=node --format=cjs --target=es2020 --allow-overwrite --outfile=lib/cli/index.js
-esbuild lib/config/index.js --bundle --log-level=error --minify --external:@serverless-devs/core  --platform=node --format=cjs --target=es2020 --allow-overwrite --outfile=lib/config/index.js
-esbuild lib/config/add/index.js --bundle --log-level=error --minify --external:@serverless-devs/core  --platform=node --format=cjs --target=es2020 --allow-overwrite --outfile=lib/config/add/index.js
-esbuild lib/config/delete/index.js --bundle --log-level=error --minify --external:@serverless-devs/core  --platform=node --format=cjs --target=es2020 --allow-overwrite --outfile=lib/config/delete/index.js
-esbuild lib/config/get/index.js --bundle --log-level=error --minify --external:@serverless-devs/core  --platform=node --format=cjs --target=es2020 --allow-overwrite --outfile=lib/config/get/index.js
-esbuild lib/set/index.js --bundle --log-level=error --minify --external:@serverless-devs/core  --platform=node --format=cjs --target=es2020 --allow-overwrite --outfile=lib/set/index.js
-esbuild lib/set/registry/index.js --bundle --log-level=error --minify --external:@serverless-devs/core  --platform=node --format=cjs --target=es2020 --allow-overwrite --outfile=lib/set/registry/index.js
-esbuild lib/set/locale/index.js --bundle --log-level=error --minify --external:@serverless-devs/core  --platform=node --format=cjs --target=es2020 --allow-overwrite --outfile=lib/set/locale/index.js
-esbuild lib/set/analysis/index.js --bundle --log-level=error --minify --external:@serverless-devs/core  --platform=node --format=cjs --target=es2020 --allow-overwrite --outfile=lib/set/analysis/index.js
-esbuild lib/clean/index.js --bundle --log-level=error --minify --external:@serverless-devs/core  --platform=node --format=cjs --target=es2020 --allow-overwrite --outfile=lib/clean/index.js
-esbuild lib/update-notifier/index.js --bundle --log-level=error --minify --external:@serverless-devs/core  --platform=node --format=cjs --target=es2020 --allow-overwrite --outfile=lib/update-notifier/index.js
+esbuild lib/index.js --bundle --log-level=error --minify --external:@serverless-devs/core  --platform=node --format=cjs --target=node10.4 --allow-overwrite --outfile=lib/index.js
+esbuild lib/init/index.js --bundle --log-level=error --minify --external:@serverless-devs/core  --platform=node --format=cjs --target=node10.4 --allow-overwrite --outfile=lib/init/index.js
+esbuild lib/cli/index.js --bundle --log-level=error --minify --external:@serverless-devs/core  --platform=node --format=cjs --target=node10.4 --allow-overwrite --outfile=lib/cli/index.js
+esbuild lib/config/index.js --bundle --log-level=error --minify --external:@serverless-devs/core  --platform=node --format=cjs --target=node10.4 --allow-overwrite --outfile=lib/config/index.js
+esbuild lib/config/add/index.js --bundle --log-level=error --minify --external:@serverless-devs/core  --platform=node --format=cjs --target=node10.4 --allow-overwrite --outfile=lib/config/add/index.js
+esbuild lib/config/delete/index.js --bundle --log-level=error --minify --external:@serverless-devs/core  --platform=node --format=cjs --target=node10.4 --allow-overwrite --outfile=lib/config/delete/index.js
+esbuild lib/config/get/index.js --bundle --log-level=error --minify --external:@serverless-devs/core  --platform=node --format=cjs --target=node10.4 --allow-overwrite --outfile=lib/config/get/index.js
+esbuild lib/set/index.js --bundle --log-level=error --minify --external:@serverless-devs/core  --platform=node --format=cjs --target=node10.4 --allow-overwrite --outfile=lib/set/index.js
+esbuild lib/set/registry/index.js --bundle --log-level=error --minify --external:@serverless-devs/core  --platform=node --format=cjs --target=node10.4 --allow-overwrite --outfile=lib/set/registry/index.js
+esbuild lib/set/locale/index.js --bundle --log-level=error --minify --external:@serverless-devs/core  --platform=node --format=cjs --target=node10.4 --allow-overwrite --outfile=lib/set/locale/index.js
+esbuild lib/set/analysis/index.js --bundle --log-level=error --minify --external:@serverless-devs/core  --platform=node --format=cjs --target=node10.4 --allow-overwrite --outfile=lib/set/analysis/index.js
+esbuild lib/clean/index.js --bundle --log-level=error --minify --external:@serverless-devs/core  --platform=node --format=cjs --target=node10.4 --allow-overwrite --outfile=lib/clean/index.js
+esbuild lib/update-notifier/index.js --bundle --log-level=error --minify --external:@serverless-devs/core  --platform=node --format=cjs --target=node10.4 --allow-overwrite --outfile=lib/update-notifier/index.js
 
