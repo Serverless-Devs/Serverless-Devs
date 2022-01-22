ac3eb3bdaf349bd6849e8e8fc82d0a81dee82853
xsahxl <xsahxl@126.com>
2022-01-22 17:47:22 +0800

    feat: 🎸 special command

--- src/special-commad/index.ts
+++ src/special-commad/index.ts
@@ -1,36 +1,53 @@
 import { CommanderStatic, Command } from '@serverless-devs/commander';
 import core from '../utils/core';
-import { isEmpty, includes } from 'lodash';
+import { isEmpty, includes, keys, get } from 'lodash';
 import { COMMAND_LIST } from '../constant';
 
-const { getGlobalArgs, parse } = core;
+const { getGlobalArgs, parse, getTemplatePath, getYamlContent } = core;
 
 class SpecialCommad {
   constructor(private command: CommanderStatic) {}
   async init() {
     if (includes(COMMAND_LIST, process.argv[2])) return;
-    const params = getGlobalArgs(process.argv.slice(2));
-    const { _: rawData, template, access, 'skip-actions': skipActions, debug } = params;
+    const argv = getGlobalArgs(process.argv.slice(2));
+    const { _: rawData, template, access, 'skip-actions': skipActions, debug } = argv;
     if (isEmpty(rawData)) return;
-    if (rawData.length === 1) {
-      const sub = new Command(rawData[0]);
-      sub.allowUnknownOption();
-      this.command.addCommand(sub);
-      await parse({
-        syaml: template,
-        method: rawData[0],
-        args: process.argv.slice(2),
-        globalArgs: {
-          access,
-          skipActions,
-          debug,
-        },
-      });
-    }
+    const sub = new Command(rawData[0]);
+    sub.allowUnknownOption();
+    this.command.addCommand(sub);
+    const { serverName, method } = await this.getParams(argv);
+    await parse({
+      syaml: template,
+      serverName,
+      method,
+      args: process.argv.slice(2),
+      globalArgs: {
+        access,
+        skipActions,
+        debug,
+      },
+    });
+  }
 
+  async getParams(argv): Promise<{ method: string; serverName?: string }> {
+    const { _: rawData, template } = argv;
+    if (rawData.length === 1) {
+      return {
+        method: rawData[0],
+      };
+    }
     if (rawData.length === 2) {
-      const sub = new Command(params._[0]);
-      this.command.addCommand(sub);
+      const spath = await getTemplatePath(template);
+      const yamlData = await getYamlContent(spath);
+      const servicesName = keys(get(yamlData, 'services'));
+      return includes(servicesName, rawData[0])
+        ? {
+            serverName: rawData[0],
+            method: rawData[1],
+          }
+        : {
+            method: rawData[0],
+          };
     }
   }
 }
