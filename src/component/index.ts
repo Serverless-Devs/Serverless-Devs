46093aa8a1cea0ab631dd92b5f5cb38f4ac64eb6
xsahxl <xsahxl@126.com>
2022-12-22 16:17:31 +0800

    feat: ##617 s component

--- src/component/index.ts
+++ src/component/index.ts
@@ -1,4 +1,3 @@
-import program from '@serverless-devs/commander';
 import core from '../utils/core';
 import i18n from '../utils/i18n';
 import path from 'path';
@@ -18,14 +17,6 @@ const description = `Get details of installed components.
 ${emoji('📖')} Document: ${colors.underline(
   'https://github.com/Serverless-Devs/Serverless-Devs/tree/master/docs/zh/command/component.md',
 )}`;
-const command = program
-  .name('s component')
-  .usage('[options]')
-  .option('--component [componentName]', 'Gets the specified component information (like: fc, fc@0.0.1)')
-  .helpOption('-h, --help', i18n('display_help_for_command'))
-  .description(description)
-  .addHelpCommand(false)
-  .parse(process.argv);
 
 async function getComponent(filePath: string) {
   const data = await getYamlContent(path.join(filePath, 'publish.yaml'));
@@ -47,149 +38,165 @@ function notFound(args) {
   });
 }
 
-(async () => {
-  const sPath = getRootHome();
-  const componentsPath = path.join(sPath, 'components');
-  const devsappPath = path.join(componentsPath, 'devsapp.cn');
-  const githubPath = path.join(componentsPath, 'github.com');
+function run(program) {
+  const command = program
+    .command('component')
+    .usage('[options]')
+    .option('--component <componentName>', 'Gets the specified component information (like: fc, fc@0.0.1)')
+    .helpOption('-h, --help', i18n('display_help_for_command'))
+    .description(description)
+    .addHelpCommand(false)
+    .action(async options => {
+      await doAction(options);
+    });
 
-  if (process.argv.length === 2) {
-    // s源
-    if (fs.existsSync(devsappPath)) {
-      const devsappDirs = fs.readdirSync(devsappPath);
-      const serverlessRows = [
-        {
-          Component: 'Component',
-          Version: 'Version',
-          Size: `Size`,
-          Description: 'Description',
-        },
-      ];
-      for (const fileName of devsappDirs) {
-        if (fileName === 'devsapp') {
-          const devsappSubPath = path.join(devsappPath, fileName);
-          const devsappSubDirs = fs.readdirSync(devsappSubPath);
-          for (const devsappFileName of devsappSubDirs) {
-            const filePath = path.join(devsappSubPath, devsappFileName);
+  const doAction = async options => {
+    const sPath = getRootHome();
+    const componentsPath = path.join(sPath, 'components');
+    const devsappPath = path.join(componentsPath, 'devsapp.cn');
+    const githubPath = path.join(componentsPath, 'github.com');
+    const rawData = process.argv.slice(2);
+
+    if (rawData.length === 1) {
+      // s源
+      if (fs.existsSync(devsappPath)) {
+        const devsappDirs = fs.readdirSync(devsappPath);
+        const serverlessRows = [
+          {
+            Component: 'Component',
+            Version: 'Version',
+            Size: `Size`,
+            Description: 'Description',
+          },
+        ];
+        for (const fileName of devsappDirs) {
+          if (fileName === 'devsapp') {
+            const devsappSubPath = path.join(devsappPath, fileName);
+            const devsappSubDirs = fs.readdirSync(devsappSubPath);
+            for (const devsappFileName of devsappSubDirs) {
+              const filePath = path.join(devsappSubPath, devsappFileName);
+              const data = await getComponent(filePath);
+              if (data.isComponent) {
+                const size = await getFolderSize(filePath);
+                serverlessRows.push({
+                  Component: `devsapp/${devsappFileName}`,
+                  Version: data.Version,
+                  Size: `${(size / 1000 / 1000).toFixed(2)} MB`,
+                  Description: data.Description,
+                });
+              }
+            }
+          } else {
+            const filePath = path.join(devsappPath, fileName);
             const data = await getComponent(filePath);
             if (data.isComponent) {
               const size = await getFolderSize(filePath);
               serverlessRows.push({
-                Component: `devsapp/${devsappFileName}`,
+                Component: data.Name,
                 Version: data.Version,
                 Size: `${(size / 1000 / 1000).toFixed(2)} MB`,
                 Description: data.Description,
               });
             }
           }
-        } else {
-          const filePath = path.join(devsappPath, fileName);
-          const data = await getComponent(filePath);
-          if (data.isComponent) {
-            const size = await getFolderSize(filePath);
-            serverlessRows.push({
-              Component: data.Name,
-              Version: data.Version,
-              Size: `${(size / 1000 / 1000).toFixed(2)} MB`,
-              Description: data.Description,
-            });
-          }
         }
+        logger.log(`\n${emoji('🔎')} serverless registry [http://registry.devsapp.cn/simple]\n`);
+        const data = new tableLayout(JSON.parse(JSON.stringify(serverlessRows)));
+        logger.log(data.toString());
       }
-      logger.log(`\n${emoji('🔎')} serverless registry [http://registry.devsapp.cn/simple]\n`);
-      const data = new tableLayout(JSON.parse(JSON.stringify(serverlessRows)));
-      logger.log(data.toString());
-    }
-    // github源
-    if (fs.existsSync(githubPath)) {
-      const githubDirs = fs.readdirSync(githubPath);
-      const githubRows = [
-        {
-          Component: 'Component',
-          Version: 'Version',
-          Size: `Size`,
-          Description: 'Description',
-        },
-      ];
-      for (const fileName of githubDirs) {
-        const githubSubPath = path.join(githubPath, fileName);
-        const githubSubDirs = fs.readdirSync(githubSubPath);
-        for (const githubFileName of githubSubDirs) {
-          const filePath = path.join(githubSubPath, githubFileName);
-          const data = await getComponent(filePath);
-          if (data.isComponent) {
-            const size = await getFolderSize(filePath);
-            githubRows.push({
-              Component: `${fileName}/${data.Name}`,
-              Version: data.Version,
-              Size: `${(size / 1000 / 1000).toFixed(2)} MB`,
-              Description: data.Description,
-            });
+      // github源
+      if (fs.existsSync(githubPath)) {
+        const githubDirs = fs.readdirSync(githubPath);
+        const githubRows = [
+          {
+            Component: 'Component',
+            Version: 'Version',
+            Size: `Size`,
+            Description: 'Description',
+          },
+        ];
+        for (const fileName of githubDirs) {
+          const githubSubPath = path.join(githubPath, fileName);
+          const githubSubDirs = fs.readdirSync(githubSubPath);
+          for (const githubFileName of githubSubDirs) {
+            const filePath = path.join(githubSubPath, githubFileName);
+            const data = await getComponent(filePath);
+            if (data.isComponent) {
+              const size = await getFolderSize(filePath);
+              githubRows.push({
+                Component: `${fileName}/${data.Name}`,
+                Version: data.Version,
+                Size: `${(size / 1000 / 1000).toFixed(2)} MB`,
+                Description: data.Description,
+              });
+            }
           }
         }
-      }
-      logger.log(`${emoji('🔎')} github registry [https://api.github.com/repos]\n`);
-      const data = new tableLayout(JSON.parse(JSON.stringify(githubRows)));
-      logger.log(data.toString());
-    }
-    return;
-  }
-  if (process.argv.length > 2) {
-    const args = minimist(process.argv.slice(2), {
-      string: ['component'],
-    });
-    if (args.component) {
-      const registry = getConfig('registry', 'http://registry.devsapp.cn/simple');
-      // s 源
-      if (registry === 'http://registry.devsapp.cn/simple') {
-        const filePath = path.join(devsappPath, args.component);
-        if (fs.existsSync(filePath)) {
-          const data = await getComponent(filePath);
-          if (data.isComponent) {
-            const size = await getFolderSize(filePath);
-            const outputs = {
-              Component: data.Name,
-              Reigstry: `serverless registry [${registry}]`,
-              Version: data.Version,
-              Size: `${(size / 1000 / 1000).toFixed(2)} MB`,
-              Description: data.Description,
-              Path: filePath,
-              Hompage: data.HomePage,
-            };
-            logger.output(outputs);
-            logger.log(`\n🙋 Delete the component, please use the command [s clean --component ${args.component}]`);
-          }
-        } else {
-          notFound(args);
-        }
-      }
-      // git 源
-      if (registry === 'https://api.github.com/repos') {
-        const filePath = path.join(githubPath, args.component);
-        if (fs.existsSync(filePath)) {
-          const data = await getComponent(filePath);
-          if (data.isComponent) {
-            const size = await getFolderSize(filePath);
-            const outputs = {
-              Component: data.Name,
-              Reigstry: `github registry [${registry}]`,
-              Version: data.Version,
-              Size: `${(size / 1000 / 1000).toFixed(2)} MB`,
-              Description: data.Description,
-              Path: filePath,
-              Hompage: data.HomePage,
-            };
-            logger.output(outputs);
-            logger.log(
-              `\n${emoji('🙋')} Delete the component, please use the command [s clean --component ${args.component}]`,
-            );
-          }
-        } else {
-          notFound(args);
-        }
+        logger.log(`${emoji('🔎')} github registry [https://api.github.com/repos]\n`);
+        const data = new tableLayout(JSON.parse(JSON.stringify(githubRows)));
+        logger.log(data.toString());
       }
       return;
     }
-  }
-  command.help();
-})();
+    if (rawData.length > 1) {
+      const args = minimist(process.argv.slice(2), {
+        string: ['component'],
+      });
+      if (args.component) {
+        const registry = getConfig('registry', 'http://registry.devsapp.cn/simple');
+        // s 源
+        if (registry === 'http://registry.devsapp.cn/simple') {
+          const filePath = path.join(devsappPath, args.component);
+          if (fs.existsSync(filePath)) {
+            const data = await getComponent(filePath);
+            if (data.isComponent) {
+              const size = await getFolderSize(filePath);
+              const outputs = {
+                Component: data.Name,
+                Reigstry: `serverless registry [${registry}]`,
+                Version: data.Version,
+                Size: `${(size / 1000 / 1000).toFixed(2)} MB`,
+                Description: data.Description,
+                Path: filePath,
+                Hompage: data.HomePage,
+              };
+              logger.output(outputs);
+              logger.log(`\n🙋 Delete the component, please use the command [s clean --component ${args.component}]`);
+            }
+          } else {
+            notFound(args);
+          }
+        }
+        // git 源
+        if (registry === 'https://api.github.com/repos') {
+          const filePath = path.join(githubPath, args.component);
+          if (fs.existsSync(filePath)) {
+            const data = await getComponent(filePath);
+            if (data.isComponent) {
+              const size = await getFolderSize(filePath);
+              const outputs = {
+                Component: data.Name,
+                Reigstry: `github registry [${registry}]`,
+                Version: data.Version,
+                Size: `${(size / 1000 / 1000).toFixed(2)} MB`,
+                Description: data.Description,
+                Path: filePath,
+                Hompage: data.HomePage,
+              };
+              logger.output(outputs);
+              logger.log(
+                `\n${emoji('🙋')} Delete the component, please use the command [s clean --component ${args.component}]`,
+              );
+            }
+          } else {
+            notFound(args);
+          }
+        }
+        return;
+      }
+    }
+    command.help();
+  };
+}
+
+export = run;
