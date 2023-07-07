3f1b932ec81317690976109700abd167fed90d7f
xsahxl <xsahxl@126.com>
2023-07-07 17:34:20 +0800

    feat: ✨ add custom use3x

--- src/command/custom/index.ts
+++ src/command/custom/index.ts
@@ -4,11 +4,13 @@ import { parseArgv } from '@serverless-devs/utils';
 import loadComponent from '@serverless-devs/load-component';
 import { get, isEmpty, each, find, first } from 'lodash';
 import ParseSpec, { IOutput } from '@serverless-devs/parse-spec';
+import { execCommand } from '@serverless-devs/core';
 import logger from '../../logger';
 import yaml from 'js-yaml';
 import { HandleError } from '../../error';
 
 export default class Custom {
+  private spec: Record<string, any> = {};
   constructor(private program: Command) { }
   async init() {
     const argv = process.argv.slice(2);
@@ -16,10 +18,16 @@ export default class Custom {
     // 工具内置命令不处理
     const systemCommandNames = this.program.commands.map(command => command.name());
     if (systemCommandNames.includes(raw[0])) return;
+    this.spec = this.parseSpec();
+    if (isEmpty(this.spec)) return;
     // help，无二级指令，二级指令为help
     if (help || raw.length === 0 || raw[0] === 'help') return await this.showHelp();
-    // engine
+    const use3x = String(get(this.spec, 'yaml.content.edition')) === '3.0.0';
+    logger.debug(`use3x: ${use3x}`);
     this.program.command(raw[0]).allowUnknownOption().action(async () => {
+      // core
+      if (!use3x) return await this.doExecCommand();
+      // engine
       const engine = new Engine({
         template,
         logConfig: {
@@ -31,10 +39,30 @@ export default class Custom {
       logger.loggerInstance.__clear();
     })
   }
+  async doExecCommand() {
+    const argv = process.argv.slice(2);
+    const { template, help, access, skipActions, debug, output } = parseArgv(argv);
+    try {
+      return await execCommand({
+        syaml: template,
+        serverName: this.spec.projectName,
+        method: this.spec.method,
+        args: process.argv.slice(2),
+        globalArgs: {
+          access,
+          skipActions,
+          debug,
+          help,
+          output,
+        },
+      });
+    } catch (error) {
+      HandleError(error);
+    }
+  }
+  // TODO: 新老版本 帮助信息是否可以用同一套。
   async showHelp() {
-    const spec = this.parseSpec();
-    if (isEmpty(spec)) return;
-    const { projectName, steps, components } = spec;
+    const { projectName, steps, components } = this.spec;
     if (isEmpty(steps)) return;
     const helpInfo = [
       'Custom Commands',
