47e73981817e0fb15388a7eb5cff7c84dbc00c9a
xsahxl <xsahxl@126.com>
2023-08-22 14:46:37 +0800

    feat: error
    Signed-off-by: xsahxl <xsahxl@126.com>

--- src/error/index.ts
+++ src/error/index.ts
@@ -43,6 +43,7 @@ const handleError = async (error: IEngineError | IEngineError[]) => {
       { key: 'Feedback:', value: chalk.cyan.underline('https://feedback.serverless-devs.com') },
     ]),
   );
+  if (process.env.NODE_ENV === 'test') return;
   process.exit(exitCode);
 };
 
