44e72333629a2244b1de4419fd02e46a6641f77c
xsahxl <xsahxl@126.com>
2023-09-06 13:51:17 +0800

    fix: test config ci
    Signed-off-by: xsahxl <xsahxl@126.com>

--- __tests__/config.test.ts
+++ __tests__/config.test.ts
@@ -2,8 +2,16 @@ import { spawnSync } from 'child_process';
 import path from 'path';
 const s = path.resolve(__dirname, '../bin/s');
 
-test('s config get from .env', async () => {
-  const res = spawnSync(s, ['config', 'get'], { cwd: __dirname });
+test('s config get from env', async () => {
+  const res = spawnSync(s, ['config', 'get'], {
+    cwd: __dirname,
+    env: {
+      ...process.env,
+      AccountID: '123',
+      AccessKeyID: '123',
+      AccessKeySecret: '123'
+    }
+  });
   const stdout = res.stdout.toString();
   console.log(stdout);
   expect(stdout).toContain('$system_environment_access');
