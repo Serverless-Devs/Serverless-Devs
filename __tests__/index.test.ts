fd7da7c2bbf5c6678c99ce4ca04ac2030220bcd7
xsahxl <xsahxl@126.com>
2023-10-20 16:15:11 +0800

    feat: support verify from component
    Signed-off-by: xsahxl <xsahxl@126.com>

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -41,5 +41,15 @@ test('--output-file', async () => {
   expect(fs.existsSync(outputFile)).toBeTruthy();
 });
 
+
+test('verify from component', async () => {
+  const dest = path.join(__dirname, './fixtures/basic');
+  const template = path.join(dest, 'verify.yaml');
+  const res = spawnSync(s, ['plan', '-t', template], { cwd });
+  const stdout = res.stdout.toString();
+  console.log(stdout);
+  expect(res.status).toBe(0);
+});
+
 // sl cli fc api ListServices -o json --output-file o.json
 // sl cli fc3@dev layer list -a shl --region cn-hangzhou -o json --output-file o.json
