112e6855cb5c9fa04cf60e2118889cee6e189ed4
zxypro1 <1018995004@qq.com>
2024-04-28 17:38:03 +0800

    fix workflow
    Signed-off-by: zxypro1 <1018995004@qq.com>

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -2,11 +2,12 @@ import { spawnSync } from 'child_process';
 import path from 'path';
 import fs from 'fs-extra';
 import * as utils from '@serverless-devs/utils';
-const s = path.resolve(__dirname, '../bin/s');
+const s = path.resolve(__dirname, process.platform === 'win32' ? '../bin/s.cmd' : '../bin/s');
 const pkg = require('../package.json');
 const cwd = path.resolve(__dirname, './fixtures/basic');
 
 test('s -v', async () => {
+  spawnSync(s, ['emptyarray'], { cwd });
   const res = spawnSync(s, ['-v']);
   const stdout = res.stdout.toString();
   console.log(stdout);
