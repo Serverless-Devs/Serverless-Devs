c883dc71ee154f96f67dacadfd7f21bb2be11792
Zone Tome <1018995004@qq.com>
2024-05-08 21:27:05 +0800

    fix publish workflow (#824)
    Signed-off-by: zxypro1 <1018995004@qq.com>

---
+++ .github/change-package-name-to-s.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+
+const packageJsonPath = path.resolve(__dirname, '../package.json');
+
+// 读取 package.json 文件
+const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
+
+// 更新 name 属性
+packageJson.name = '@serverless-devs/s';
+
+// 将更新后的对象写回 package.json 文件
+fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2) + '\n');
